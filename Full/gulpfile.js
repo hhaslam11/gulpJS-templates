12f56ef5bac9c06c8043b6079ba833d2dfd620c5
Kaleb <kalebmarc@hotmail.com>
2017-10-10 20:37:13 -0700

    Reorganized gulpfile, added a build task, and changed default task to del_dist before building

--- Full/gulpfile.js
+++ Full/gulpfile.js
@@ -6,25 +6,6 @@ var clean_html = require('gulp-htmlmin');
 var clean_css = require('gulp-clean-css');
 var clean_js = require('gulp-minify');
 
-//Put's bootstrap files in dist folder
-gulp.task('bootstrap', function(){
-  //css
-  gulp.src(['node_modules/bootstrap/less/bootstrap.less', 'src/less/*.less'])
-    .pipe(less())
-    .pipe(clean_css({compatibility: 'ie8'}))
-    .pipe(gulp.dest("dist/css"));
-
-  //js
-  gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
-    .pipe(gulp.dest("dist/js"));
-});
-
-//Deletes everything in dist/
-//recommended for a fresh build of the site.
-gulp.task('del-dist', function(){
-	return del(['dist/']);
-});
-
 //Compiles and minifies all LESS files from src/css/ to dist/css/
 gulp.task('less', function(){
     return gulp.src(['./src/**/*.less'])
@@ -60,13 +41,40 @@ gulp.task('other_files', function(){
     .pipe(gulp.dest('./dist/'));
 });
 
-//Runs all the tasks mandatory to building the website
-gulp.task('default', function(){
-	run_seq(
+//Put's bootstrap files in dist folder
+gulp.task('bootstrap', function(){
+  //css
+  gulp.src(['node_modules/bootstrap/less/bootstrap.less', 'src/less/*.less'])
+    .pipe(less())
+    .pipe(clean_css({compatibility: 'ie8'}))
+    .pipe(gulp.dest("dist/css"));
+
+  //js
+  gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
+    .pipe(gulp.dest("dist/js"));
+});
+
+//Deletes everything in dist/
+//recommended for a fresh build of the site.
+gulp.task('del-dist', function(){
+	return del(['dist/']);
+});
+
+//Runs tasks needed to build the website into /dist/
+gulp.task('build', function(){
+  run_seq(
     'less',
 	  'js',
     'html',
 		'other_files',
 	  'bootstrap'
+  );
+});
+
+//Deletes dist/ folder for a clean build, then builds the site
+gulp.task('default', function(){
+	run_seq(
+    'del-dist',
+    'build'
   );
 });
